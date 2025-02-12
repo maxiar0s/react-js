50a53438deb3a7a3c5686b4864b52884095f518f
Maximiliano Aros <maxi.aros.osorio@gmail.com>
2025-02-12 11:30:24 -0300

    local-storage

--- react-exercise-1/src/App.jsx
+++ react-exercise-1/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect, use } from "react";
 import confetti from "canvas-confetti";
 import Square from "./components/Square";
 import WinnerModal from "./components/WinnerModal";
@@ -7,8 +7,19 @@ import { TURNS } from "./constants";
 import { checkWinnerFrom, checkEndGame } from "./logic/board";
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [turn, setTurn] = useState(TURNS.X);
+  //console.log("render");
+
+  const [board, setBoard] = useState(() => {
+    //console.log("inicializar estado de board");
+    const boardFromStorage = window.localStorage.getItem("board");
+    return boardFromStorage
+      ? JSON.parse(boardFromStorage)
+      : Array(9).fill(null);
+  });
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem("turn");
+    return turnFromStorage ? turnFromStorage : TURNS.X;
+  });
   const [winner, setWinner] = useState(null); // null=ningun ganador, false=empate, x/o=ganador
 
   const updateBoard = (index) => {
@@ -23,6 +34,10 @@ function App() {
     //cambiar de turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+
+    //guardar partida
+    window.localStorage.setItem("board", JSON.stringify(newBoard));
+    window.localStorage.setItem("turn", newTurn);
 
     //verificar si hay un ganador
     const newWinner = checkWinnerFrom(newBoard);
@@ -41,7 +56,15 @@ function App() {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
+
+    //remover partida guardada
+    window.localStorage.removeItem("board");
+    window.localStorage.removeItem("turn");
   };
+
+  useEffect(() => {
+    console.log("useEffect");
+  }, [winner]);
 
   return (
     <main className="board">
